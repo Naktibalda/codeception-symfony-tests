730fa8022a6bb7840310fb22bc977d76488797b1
Gintautas Miselis <gintautas@miselis.lt>
2017-06-18 22:40:51 +0100

    Copied tests from codeception-zf2-tests

---
+++ tests/doctrine/createTwoUsersAndGetListCept.php
@@ -0,0 +1,24 @@
+<?php 
+$I = new DoctrineTester($scenario);
+$I->wantTo('check that the entities persist across multiple steps');
+if (!method_exists('Zend\ServiceManager\ServiceLocatorInterface', 'addPeeringServiceManager')) {
+    $scenario->skip('This test is incompatible with ZF3');
+}
+
+$I->amOnPage('/doctrine/');
+$I->seeResponseCodeIs(200);
+$I->see('0 users in the table');
+
+$I->fillField('name', 'John Doe');
+$I->click('Create User');
+
+$I->amOnPage('/doctrine/');
+$I->see('1 users in the table');
+
+$I->fillField('name', 'John Smith');
+$I->click('Create User');
+
+$I->amOnPage('/doctrine/');
+$I->see('2 users in the table');
+
+$I->seeInRepository('\Application\Entity\User', ['name' => 'John Doe']);
