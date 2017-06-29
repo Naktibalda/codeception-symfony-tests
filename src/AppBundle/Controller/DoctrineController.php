80c2da4f6476d4d037572f24b10907d1dc6fa8df
Gintautas Miselis <gintautas@miselis.lt>
2017-06-29 21:39:27 +0100

    Implemented DoctrineController and User entity

--- src/AppBundle/Controller/DoctrineController.php
+++ src/AppBundle/Controller/DoctrineController.php
@@ -6,15 +6,32 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 class DoctrineController extends Controller
 {
     /**
      * @Route("/doctrine", name="doctrine.index")
      */
-    public function indexAction()
+    public function indexAction(EntityManagerInterface $em)
     {
-        return new Response('users in the table');
+        $query = $em->createQuery('SELECT COUNT(u) FROM AppBundle\Entity\User u');
+        $count = $query->getSingleScalarResult();
+        return $this->render('doctrine/index.html.twig', [
+            'count' => $count,
+        ]);
+    }
+
+    /**
+     * @Route("/doctrine/create-user", name="doctrine.create-user")
+     */
+    public function createUserAction(EntityManagerInterface $em, Request $request)
+    {
+        $user = new \AppBundle\Entity\User();
+        $user->name = $request->request->get('name');
+        $em->persist($user);
+        $em->flush();
+        return new Response('User created');
     }
 }
