35a6df1c69d8f12f2577be73b2d0b8f4f25152da
Gintautas Miselis <gintautas@miselis.lt>
2017-06-30 21:39:36 +0100

    Downgraded to support symfony 3.2

--- src/AppBundle/Controller/DoctrineController.php
+++ src/AppBundle/Controller/DoctrineController.php
@@ -14,8 +14,9 @@ class DoctrineController extends Controller
     /**
      * @Route("/doctrine", name="doctrine.index")
      */
-    public function indexAction(EntityManagerInterface $em)
+    public function indexAction()
     {
+        $em = $this->get('doctrine')->getManager();
         $query = $em->createQuery('SELECT COUNT(u) FROM AppBundle\Entity\User u');
         $count = $query->getSingleScalarResult();
         return $this->render('doctrine/index.html.twig', [
@@ -26,8 +27,9 @@ class DoctrineController extends Controller
     /**
      * @Route("/doctrine/create-user", name="doctrine.create-user")
      */
-    public function createUserAction(EntityManagerInterface $em, Request $request)
+    public function createUserAction(Request $request)
     {
+        $em = $this->get('doctrine')->getManager();
         $user = new \AppBundle\Entity\User();
         $user->name = $request->request->get('name');
         $em->persist($user);
